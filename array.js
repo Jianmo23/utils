5a038190d48d0de0ca46d20163d1bc95c39a374c
朱杨杨 <zhuyangyang@cashbus.com>
2019-05-05 18:14:42 +0800

    数组扁平化 flat

--- array.js
+++ array.js
@@ -1,6 +1,8 @@
 /*
  * 数组常用方法
  */
+ import {isArray} from './determine-determine-type';
+
 var toArray = (arraylike, startIndex) => !!Array.from && (Array.from(arraylike) || []).slice(startIndex) || [].slice.call(arraylike, startIndex);
 
 var each = (arraylike, fn) => {
@@ -15,7 +17,18 @@ var each = (arraylike, fn) => {
     }
 };
 
+var flat = (arr = []) => {
+    if ([].flat) {
+        return arr.flat(Infinity);
+    } else {
+        return arr.reduce((init, cur) => {
+            return isArray(cur) ? init.concat(flat(cur)) : init.concat(cur);
+        }, []);
+    }
+};
+
 export {
     toArray,
-    each
+    each,
+    flat
 }

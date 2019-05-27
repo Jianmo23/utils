96806780c38a6ce31d6d5e0592cac600555c7e69
朱杨杨 <zhuyangyang@cashbus.com>
2019-05-27 16:24:14 +0800

    变量名纠正

--- src/formatter/formatter.js
+++ src/formatter/formatter.js
@@ -1,7 +1,7 @@
 // 常用格式化方法
 // 格式化金钱
-const formatteMoney = money => money && money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+const formatMoney = money => money && money.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 
 export {
-    formatteMoney
+    formatMoney
 }
